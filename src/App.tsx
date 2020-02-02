6bea410c58f3c1b18b4a7929dd817cc0f424f4cd
Cem Derin <cem@wysiwyg.de>
2020-02-02 18:37:46 +0100

    feat: 🎸 Load attackers and defenders from battlescribe cat file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import BSString from '@cemderin/battlescribe-to-json/dist/src/BSString';
+import { Model } from '@cemderin/battle-calculator';
 
 const App: React.FC = () => {
+  const [attackers, setAttackers] = useState<Model[]>([]);
+  const [defenders, setDefenders] = useState<Model[]>([]);
+
+  const fileChangeHandler = (files: any, areAttackers: boolean = true) => {
+    for (let file of files) {
+      let reader = new FileReader();
+      reader.onload = (event: any) => {
+        const BS = new BSString(event.target.result);
+        BS.load().then(() => {
+          const models = BS.extractModels();
+          if (areAttackers) setAttackers(models);
+          if (!areAttackers) setDefenders(models);
+        });
+      }
+
+      reader.readAsText(file);
+    }
+  }
+
   return (
     <div>
-      battle-calculartor-ui
+      <input type="file" onChange={(e) => {
+        fileChangeHandler(e.target.files)
+      }} />
+
+      <input type="file" onChange={(e) => {
+        fileChangeHandler(e.target.files, false)
+      }} />
+
+      <h3>Attackers</h3>
+      {attackers.map(_a => {
+        return <div>
+          {_a.name}
+        </div>
+      })}
+
+      <h3>Defenders</h3>
+      {defenders.map(_a => {
+        return <div>
+          {_a.name}
+        </div>
+      })}
     </div>
   );
 }

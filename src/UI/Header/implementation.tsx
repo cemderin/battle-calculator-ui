500b2a1a8a98acfb7f3f79336eabe4d79e3026cb
Cem Derin <cem@wysiwyg.de>
2020-02-03 21:14:16 +0100

    feat: 🎸 Perform calculations completely with loaded data

--- src/UI/Header/implementation.tsx
+++ src/UI/Header/implementation.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import BSString from '@cemderin/battlescribe-to-json/dist/src/BSString';
 
 const UIHeader: React.FC = (props: any) => {
+    console.log(props.factions);
     const loadData = () => {
         // fetch data from github: https://api.github.com/repos/BSData/wh40k-killteam/contents
         fetch('https://api.github.com/repos/BSData/wh40k-killteam/contents').then((result: any) => {
@@ -8,13 +10,38 @@ const UIHeader: React.FC = (props: any) => {
         }).then((result: any) => {
             const factions = result.filter((item: any) => {
                 let nameSegments = item.name.split('.');
-                let suffix = nameSegments[nameSegments.length -1];
-                if(suffix === 'cat') return true;
+                let suffix = nameSegments[nameSegments.length - 1];
+                if (suffix === 'cat') return true;
                 return false;
             });
 
-            console.log('factions', factions);
-            props.setFactions(factions);
+            const promises = factions.map((faction: any) => {
+                return fetch(faction.download_url).then((result: any) => {
+                    return result.text();
+                }).then((result: any) => {
+                    return faction.download_url_response = result;
+                });
+            });
+
+            Promise.all(promises).then((result: any) => {
+
+                const modelPromises = factions.map((faction: any) => {
+                    const parser = new BSString(faction.download_url_response);
+                    return parser.load().then(() => {
+                        try {
+                            const models = parser.extractModels();
+                            return faction.models = models;
+                        } catch(e) {
+                            return faction
+                        } 
+                    }); 
+                });
+
+                Promise.all(modelPromises).then((result: any) => {
+                    console.log('factions', factions);
+                    props.setFactions(factions);
+                })
+            });
         })
     }
 
@@ -29,7 +56,9 @@ const UIHeader: React.FC = (props: any) => {
             <button onClick={loadData}>Load data</button>
         )}
 
-        <button onClick={reset}>Reset</button>
+        {props.factions.length > 0 && (
+            <button onClick={reset}>Reset</button>
+        )}
     </header>
 }
 
--- src/UI/implementation.tsx
+++ src/UI/implementation.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createGlobalStyle } from 'styled-components'
 import UIHeader from './Header';
+import Phase from '@cemderin/battle-calculator';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -10,24 +11,90 @@ const GlobalStyle = createGlobalStyle`
 `
 
 const UI: React.FC = (props: any) => {
+    const [attackerIndex, setAttackerIndex] = useState(-1);
+    const [defenderIndex, setDefenderIndex] = useState(-1);
+
+    const calc = () => {
+        let results: Array<any> = [];
+
+        for(let attacker of props.factions[attackerIndex].models) {
+            for(let defender of props.factions[defenderIndex].models) {
+                const phase = new Phase();
+                phase.attacker = attacker;
+                phase.defender = defender;
+
+                results.push({
+                    attacker: attacker.name,
+                    defender: defender.name,
+                    result: phase.handle()
+                });
+            }
+        }
+
+        return results;
+    };
     return <React.Fragment>
         <GlobalStyle />
-
         <UIHeader />
 
         {props.factions.length > 0 && (
             <React.Fragment>
-                <select>
-                    {props.factions.map((faction: any, index: number) => {
-                        return <option key={index}>{faction.name}</option>
-                    })}
-                </select>
 
-                <select>
-                    {props.factions.map((faction: any, index: number) => {
-                        return <option key={index}>{faction.name}</option>
-                    })}
-                </select>
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th>
+                                Attacker
+                                <select onChange={(e: any) => {
+                                    setAttackerIndex(e.target.value)
+                                }}>
+                                    <option value={-1}>Select</option>
+                                    {props.factions.map((faction: any, index: number) => {
+                                        return <option key={index} value={index}>{faction.name}</option>
+                                    })}
+                                </select>
+                            </th>
+                            <th>
+                                Defender
+                                <select onChange={(e: any) => {
+                                    setDefenderIndex(e.target.value)
+                                }}>
+                                    <option value={-1}>Select</option>
+                                    {props.factions.map((faction: any, index: number) => {
+                                        return <option key={index} value={index}>{faction.name}</option>
+                                    })}
+                                </select>
+                            </th>
+                            <th>
+                                Wounds caused
+                            </th>
+                        </tr>
+                    </thead>
+
+                    {attackerIndex >= 0 && defenderIndex >= 0 && (
+                        <React.Fragment>
+                            {calc().sort((resultA: any, resultB: any) => {
+                                if(resultA.result < resultB.result) return 1;
+                                if(resultA.result > resultB.result) return -1;
+                                return 0;
+                            }).map((result: any) => {
+                                return <tr>
+                                    <td>
+                                        {result.attacker}
+                                    </td>
+                                    <td>
+                                        {result.defender}
+                                    </td>
+                                    <td>
+                                        {result.result}
+                                    </td>
+                                </tr>
+                            })}
+                        </React.Fragment>
+                    )}
+
+                </table>
             </React.Fragment>
         )}
     </React.Fragment>

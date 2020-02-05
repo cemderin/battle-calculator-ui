1c07f1b05482fb418a06efd82d31665ae18895ad
Cem Derin <cem@wysiwyg.de>
2020-02-05 23:13:12 +0100

    fix: 🐛 Fix missing global styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Provider as ReduxProvider } from "react-redux";
 import UI from './UI'; 
 import reduxStore from './store/store';
+import './App.css';
 
 const App: React.FC = () => {
 
